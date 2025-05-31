56884e091dcc87826765c68ab20098fbdab7f1ea
Attila Kerekes <akerekes81@gmail.com>
2025-05-31 18:14:58 +0100

    test: try thumbnail_url only in creative library for visual data

--- src/components/meta/CreativeAnalyticsDashboard.js
+++ src/components/meta/CreativeAnalyticsDashboard.js
@@ -515,7 +515,7 @@ const CreativeAnalyticsDashboard = () => {
         {
           params: {
             access_token: accessToken,
-            fields: 'id',
+            fields: 'thumbnail_url',
             limit: 250
           }
         }
